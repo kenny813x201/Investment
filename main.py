b01dd4474999e3577a20d47b91f910ee46f95f41
kenny813x201 <kenny813x201@gmail.com>
2020-02-17 11:56:35 +1100

    Add save pickle file

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import csv
+import pickle
 
 
-def save_csv(element):
-    """Save top 500 stocks in base on volumn"""
+def save_csv(companies):
+    """Save top 500 stocks in base on volumn to a csv file"""
     print("Saving companies.csv...")
-    companies = element.find_elements_by_xpath('./tbody/tr/td[2]/a')
 
     Path("output").mkdir(parents=True, exist_ok=True)
     file_name = 'output/companies.csv'
@@ -24,12 +24,36 @@ def save_csv(element):
             url = company.get_attribute('href')
             writer.writerow([name, url])
             i = i + 1
+    
+    print('companies.csv created')
 
+def save_pickle(companies):
+    """Save top 500 stocks in base on volumn to pickle file"""
+    print("Saving companies.pickle...")
+
+    Path("output").mkdir(parents=True, exist_ok=True)
+    file_name = 'output/companies.pickle'
+
+    companies_dict = {}
+    i = 0
+    while i < 500:
+        company = companies[i]
+        companies_dict[company.text] = {
+            "name": company.text,
+            "url": company.get_attribute('href'),
+        }
+        i += 1
+
+    with open(file_name, 'wb') as handle:
+        pickle.dump(companies_dict, handle)
+
+    print('companies.pickle created')
+       
 
 if __name__ == "__main__":
 
     options = webdriver.ChromeOptions()
-    # options.add_argument("headless")
+    options.add_argument("headless")
     driver = webdriver.Chrome(options=options)
 
     driver.get("https://au.investing.com/equities/australia")
@@ -47,8 +71,9 @@ if __name__ == "__main__":
         driver.implicitly_wait(5)
         driver.find_element_by_xpath('//th[@data-col-caption="Vol"]')
         driver.implicitly_wait(5)
-        save_csv(element)
-        print('companies.csv created')
+        companies = element.find_elements_by_xpath('./tbody/tr/td[2]/a')
+        # save_csv(companies)
+        save_pickle(companies)
     finally:
         driver.quit()
     driver.quit()
